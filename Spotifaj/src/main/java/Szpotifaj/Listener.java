589a9a442e0df02753d2bbe81310c9d8ef8eacb6
Patryk-Komputer\Patryk <patryk.bak@gazeta.pl>
2016-10-22 13:16:55 +0200

    Revert "działaj dzifko"
    This reverts commit 961d781efd77c2c5e4066502d5e2cb250f22c1a5.

--- Spotifaj/src/main/java/Szpotifaj/Listener.java
+++ Spotifaj/src/main/java/Szpotifaj/Listener.java
@@ -12,30 +12,37 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaPlayer.Status;
 
-public class Listener implements EventHandler {
+public class Listener implements EventHandler
+{
 	ArrayList<ToggleButton> alreadyClicked = new ArrayList<ToggleButton>();
 	Main frame;
 	Icons icons;
 	Object source;
 	CenterPanelChanger panelChanger;
 
-	Listener(Main frame, Icons icons) {
+	
+	Listener(Main frame, Icons icons)
+	{
 		this.frame = frame;
 		this.icons = icons;
 		panelChanger = new CenterPanelChanger(this.frame);
-
+		
+		
+		
 	}
-
-	void setFirstClicked() {
+	
+	void setFirstClicked()
+	{
 		alreadyClicked.add(frame.przegladaj);
 		icons.changeColor(alreadyClicked.get(0).getText(), 0);
 	}
-
 	@Override
-	public void handle(Event e) {
+	public void handle(Event e) 
+	{
 		source = e.getSource();
-
-		if (source == frame.przegladaj) {
+		
+		if(source == frame.przegladaj)
+		{
 			icons.changeColor(alreadyClicked.get(0).getText(), 1);
 			alreadyClicked.remove(0).setId("menuButtons");
 			frame.przegladaj.setId("menuButtonsClicked");
@@ -43,7 +50,8 @@ public class Listener implements EventHandler {
 			icons.changeColor(alreadyClicked.get(0).getText(), 0);
 			panelChanger.panelPrzegladaj();
 		}
-		if (source == frame.utwory) {
+		if(source == frame.utwory)
+		{
 			icons.changeColor(alreadyClicked.get(0).getText(), 1);
 			alreadyClicked.remove(0).setId("menuButtons");
 			frame.utwory.setId("menuButtonsClicked");
@@ -51,7 +59,8 @@ public class Listener implements EventHandler {
 			icons.changeColor(alreadyClicked.get(0).getText(), 0);
 			panelChanger.panelClear();
 		}
-		if (source == frame.albumy) {
+		if(source == frame.albumy)
+		{
 			icons.changeColor(alreadyClicked.get(0).getText(), 1);
 			alreadyClicked.remove(0).setId("menuButtons");
 			frame.albumy.setId("menuButtonsClicked");
@@ -59,7 +68,8 @@ public class Listener implements EventHandler {
 			icons.changeColor(alreadyClicked.get(0).getText(), 0);
 			panelChanger.panelClear();
 		}
-		if (source == frame.wykonawcy) {
+		if(source == frame.wykonawcy)
+		{
 			icons.changeColor(alreadyClicked.get(0).getText(), 1);
 			alreadyClicked.remove(0).setId("menuButtons");
 			frame.wykonawcy.setId("menuButtonsClicked");
@@ -67,7 +77,8 @@ public class Listener implements EventHandler {
 			icons.changeColor(alreadyClicked.get(0).getText(), 0);
 			panelChanger.panelClear();
 		}
-		if (source == frame.playlisty) {
+		if(source == frame.playlisty)
+		{
 			icons.changeColor(alreadyClicked.get(0).getText(), 1);
 			alreadyClicked.remove(0).setId("menuButtons");
 			frame.playlisty.setId("menuButtonsClicked");
@@ -75,22 +86,30 @@ public class Listener implements EventHandler {
 			icons.changeColor(alreadyClicked.get(0).getText(), 0);
 			panelChanger.panelClear();
 		}
-		if (source == frame.playButton) {
-
+		if(source == frame.playButton)
+		{
+			
+			
+			
+			
+			
 			Status status = frame.mediaPlayer.getStatus();
 
 			if (status == Status.PLAYING) {
-
-				frame.mediaPlayer.pause();
-				frame.playButton.setGraphic(icons.playButton);
-
+				
+			frame.mediaPlayer.pause();
+			frame.playButton.setGraphic(icons.playButton);
+						
 			} else {
-
-				frame.mediaPlayer.play();
-				frame.playButton.setGraphic(icons.pauseButton);
-
+				
+			frame.mediaPlayer.play();
+			frame.playButton.setGraphic(icons.pauseButton);
+			
+			
 			}
-
+			
+		
+			
 		}
 	}
 
